0a7d7e65ea2fe72c13468392a8cb2bb47054d5d7
Pasha <pasha@getinkworks.com>
2024-06-18 17:15:44 -0700

    removing properties from scheme validation

--- src/utils/validate-env.ts
+++ src/utils/validate-env.ts
@@ -1,9 +1,6 @@
 import { Type } from '@sinclair/typebox';
 
 export const schema = Type.Object({
-	NODE_ENV: Type.String(),
 	API_VERSION: Type.String(),
-	ORIGIN: Type.String(),
-	SECRET_KEY: Type.String(),
 	PORT: Type.String(),
 });
