9498bfd879749d9ded9a9ef631bf2be2051fdfa9
Pasha <pasha@getinkworks.com>
2024-06-25 22:42:28 -0700

    fixed route

--- src/plugins/initialize-routes.ts
+++ src/plugins/initialize-routes.ts
@@ -5,7 +5,7 @@ import { Routes } from '@interfaces/route';
 import IndexRoute from '@routes/index/route';
 import AutocompleteRoute from '@routes/autocomplete/route';
 import ClothingShopRoute from '@routes/clothing-shop/route';
-import ChessParty from '@root/src/routes/chess-party/route';
+import ChessParty from '@routes/chess-party/route';
 
 export const initializeRoutes: FastifyPluginCallbackTypebox<FastifyPluginOptions> = (server, _, done) => {
 	const routes = [new AutocompleteRoute(), new ClothingShopRoute(), new ChessParty(), new IndexRoute()];
